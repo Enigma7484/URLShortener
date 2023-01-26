7fcbd8c460e83a27dab2d4a1b6031bab11c63b73
Enigma7484 <omarhaque7484@gmail.com>
2023-01-26 18:06:00 -0500

    Commit 2
    Halfway Through

--- shortener.py
+++ shortener.py
@@ -1,16 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///urls.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 
-@app.route('/')
-def hello_world():
-    return render_template("home.html")
+class Urls(db.Model):
+    id_ = db.Column("id_", db.Integer, primary_key=True)
+    long = db.Column("long", db.String())
+    short = db.Column("short", db.String(3))
+
+    def __init__(self, long, short):
+        self.long = long
+        self.short = short
 
 
-@app.route('/omar')
-def hello_omar():
-    return "HI OMAR"
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+@app.route('/', methods=['POST', 'GET'])
+def home():
+    if request.method == "POST":
+        url_received = request.form["nm"]
+        return url_received
+    else:
+        return render_template('home.html')
 
 
 if __name__ == '__main__':
